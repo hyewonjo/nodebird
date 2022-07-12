b0f9608a00507d6882479e889ac732316be81ec5
hyewonjo <hyewonjo92@gmail.com>
2022-07-12 16:18:50 +0900

    commit

---
+++ passport/localStrategy.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+module.exports = () => {
+    passport.use(new LocalStrategy({ // 전략에 관한 설정
+        usernameField: 'email', // req.body 속성명
+        passworeField: 'password',
+    },
+        /*
+        실제 전략을 수행하는 함수. 첫번째 인수에서 넣어준 email과 password가 인수로 들어간다.
+        done 함수는 passport.authenticate 의 콜백 함수.
+         */
+    async(email, password, done) => {
+        try {
+            const exUser = await User.findOne({where: {email}});
+            if (exUser) {
+                const result = await bcrypt.compare(password, exUser.password);
+                if (result) {
+                    done(null, exUser); // 두번째 인수를 사용하지 않는 경우는 로그인에 실패했을 때뿐. 첫번째 인수를 사용하는 경우는 서버 에러 발생했을 때
+                } else {
+                    done(null, false, { message: '비밀번호가 일치하지 않습니다.'}); // 세번째 인수는 비밀번호가 일치하지 않거나 존재하지 않는 회원일 때와 같은 사용자 정의 에러 발생시. info 객체
+                }
+            } else {
+                done(null, false, { message: '가입되지 않은 회원입니다.'});
+            }
+        } catch (error) {
+            console.error(error);
+            done(error); // 서버 에러 발생했으니 첫번째 인수로 error
+        }
+    }));
+};
---
+++ routes/post.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const multer = require('multer');
+// aws에 올리기 > multer-s3
+// 클라우드 스토리지에 올리기 > multer-google-storage
+const path = require('path');
+const fs = require('fs');
+
+const { Post, Hashtag } = require('../models');
+const {isLoggedIn} = require('./middlewares');
+
+const router = express.Router();
+
+try {
+    fs.readdirSync('uploads');
+} catch (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads'); // mkdir의 동기버전
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
+    console.log(req.file);
+    res.json({ url: `/img/${req.file.filename}`});
+});
+
+const upload2 = multer();
+router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
+    try {
+        const post = await Post.create({
+            content: req.body.content,
+            img: req.body.url,
+            UserId: req.user.id,
+        });
+        const hashtags = req.body.content.match(/#[^\s#]+/g);
+        if (hashtags) {
+            const result = await Promise.all(
+                hashtags.map(tag => {
+                    return Hashtag.findOrCreate({
+                        where: { title: tag.slice(1).toLowerCase() },
+                    })
+                }),
+            );
+            await post.addHashtags(result.map(r => r[0])); // result에 [모델, 생성여부]를 반환하므로 추출해냄
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+module.exports = router;
